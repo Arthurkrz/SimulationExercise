3aece983e5926cbc5fc672af0ce674c9999f7562
Arthur K <arthurk031@outlook.com>
2025-06-02 10:53:41 -0300

    Feature/Finalização de método estático de geração de mensages

---
+++ SimulationExercise.Core/Common/LogMessages.cs
@@ -0,0 +1,18 @@
+﻿namespace SimulationExercise.Core.Common
+{
+    public static class LogMessages
+    {
+        public const string CONTINUETONEXTFILE = "Continuing to next file (if exists)...\n";
+
+        public const string EXPORTAVERAGEPROVINCEDATA = "Exporting average province data...";
+        public const string IMPORTREADING = "Importing readings...";
+        public const string CREATEOBJECT = "Creating {object}s...";
+
+        public const string OBJECTCREATIONSUCCESS = "{count} {object} created successfully!";
+
+        public const string NOREADINGIMPORTED = "No readings have been imported!";
+        public const string NOOBJECTCREATED = "No {object}s have been created!";
+        public const string NOCSVFILESFOUND = "No CSV files found in the 'IN' directory.";
+        public const string ERRORSFOUND = "Errors found! ({errorGroupNumber})";
+    }
+}
