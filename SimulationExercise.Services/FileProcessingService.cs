e0207834102411b5e926c507140df58cb7e9d603
Arthur K Laptop <arthurk031@outlook.com>
2025-06-03 22:36:19 -0300

    Refact/FinalizaçãoDeSeparaçãoDeLogicaFPS

--- SimulationExercise.Services/FileProcessingService.cs
+++ SimulationExercise.Services/FileProcessingService.cs
@@ -15,6 +15,7 @@ namespace SimulationExercise.Services
         private readonly IAverageProvinceDataFactory _averageProvinceDataFactory;
         private readonly IAverageProvinceDataExportService _averageProvinceDataExportService;
         private readonly ILogger<FileProcessingService> _logger;
+        private int _errorGroupNumber;
 
         public FileProcessingService(IReadingImportService readingImportService,
                                      IConsistentReadingFactory consistentReadingFactory,
@@ -34,12 +35,10 @@ namespace SimulationExercise.Services
         public void ProcessFile(string inDirectoryPath, string baseOutDirectoryPath)
         {
             string noErrorsFilePath = ExportDirectoryPathGeneratorAndLoggerConfiguration(baseOutDirectoryPath);
-            int errorGroupNumber = 0;
+            _errorGroupNumber = 0;
 
             if (!Directory.Exists(inDirectoryPath))
-            {
                 Directory.CreateDirectory(inDirectoryPath);
-            }
 
             var files = Directory.GetFiles(inDirectoryPath);
             if (files.Length == 0)
@@ -50,138 +49,179 @@ namespace SimulationExercise.Services
 
             foreach (var file in files)
             {
-                ImportResult importResult = null;
-                using(var str = new FileStream(file,
-                                                FileMode.Open,
-                                                FileAccess.Read))
-                {
-                    _logger.LogInformation(LogMessages.IMPORTREADING);
-                    importResult = _readingImportService.Import(str);
-                } 
+                var importReadingsResult = ImportReadings(file);
+                if (importReadingsResult.Readings.Count == 0) continue;
 
-                if (!importResult.Success)
+                var consistentReadingsResult = 
+                    CreateConsistentReadings(importReadingsResult);
+                if (!consistentReadingsResult.Success) continue;
+
+                var createProvinceDataResult = 
+                    CreateProvinceData(consistentReadingsResult.consistentReadings);
+                if (!createProvinceDataResult.Success) continue;
+
+                var createAverageProvinceDataResult = 
+                    CreateAverageProvinceData(createProvinceDataResult.provinceDatas);
+                if (!createAverageProvinceDataResult.Success) continue;
+
+                ExportAverageProvinceData(noErrorsFilePath, 
+                    createAverageProvinceDataResult.averageProvinceDatas);
+            }
+        }
+
+        public ImportResult ImportReadings(string file)
+        {
+            ImportResult importResult = null;
+            using (var str = new FileStream(file,
+                                            FileMode.Open,
+                                            FileAccess.Read))
+            {
+                _logger.LogInformation(LogMessages.IMPORTREADING);
+                importResult = _readingImportService.Import(str);
+            }
+
+            if (!importResult.Success)
+            {
+                _logger.LogError(LogMessages.ERRORSFOUND, _errorGroupNumber);
+                foreach (var error in importResult.Errors)
                 {
-                    _logger.LogError(LogMessages.ERRORSFOUND, errorGroupNumber);
-                    foreach (var error in importResult.Errors)
+                    _logger.LogError(error);
+                    continue;
+                }
+
+                _errorGroupNumber++;
+            }
+
+            if (importResult.Readings.Count == 0)
+            {
+                _logger.LogError(LogMessages.NOREADINGIMPORTED);
+                _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
+                return importResult;
+            }
+
+            _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
+                                   importResult.Readings.Count,
+                                   nameof(Reading));
+
+            return importResult; 
+        }
+
+        public (bool Success, IList<ConsistentReading> consistentReadings) CreateConsistentReadings(ImportResult importResult)
+        {
+            IList<ConsistentReading> consistentReadings =
+                new List<ConsistentReading>();
+
+            _logger.LogInformation(LogMessages.CREATEOBJECT, nameof(ConsistentReading));
+            foreach (Reading reading in importResult.Readings)
+            {
+                var cr = _consistentReadingFactory.CreateConsistentReading(reading);
+                if (!cr.Success)
+                {
+                    _logger.LogError(LogMessages.ERRORSFOUND, _errorGroupNumber);
+                    foreach (var error in cr.Errors)
                     {
                         _logger.LogError(error);
                         continue;
                     }
 
-                    errorGroupNumber++;
+                    _errorGroupNumber++;
                 }
 
-                if (importResult.Readings.Count == 0)
-                {
-                    _logger.LogError(LogMessages.NOREADINGIMPORTED);
-                    _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
-                    continue;
-                }
-
-                _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
-                                       importResult.Readings.Count,
-                                       nameof(Reading));
-
-                IList<ConsistentReading> consistentReadings = 
-                    new List<ConsistentReading>();
-
-                _logger.LogInformation(LogMessages.CREATEOBJECT, nameof(ConsistentReading));
-                foreach (Reading reading in importResult.Readings)
-                {
-                    var cr = _consistentReadingFactory.CreateConsistentReading(reading);
-                    if (!cr.Success)
-                    {
-                        _logger.LogError(LogMessages.ERRORSFOUND, errorGroupNumber);
-                        foreach (var error in cr.Errors)
-                        {
-                            _logger.LogError(error); 
-                            continue;
-                        }
-
-                        errorGroupNumber++;
-                    }
-
-                    else consistentReadings.Add(cr.Value);
-                }
-
-                if (consistentReadings.Count == 0)
-                {
-                    _logger.LogError(LogMessages.NOOBJECTCREATED, nameof(ConsistentReading));
-                    _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
-                    continue;
-                }
-
-                _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
-                                       consistentReadings.Count,
-                                       nameof(ConsistentReading));
-
-                _logger.LogInformation(LogMessages.CREATEOBJECT, nameof(ProvinceData));
-
-                IList<ProvinceData> provinceDatas =
-                    _provinceDataListFactory.CreateProvinceDataList(consistentReadings); 
-
-                if (provinceDatas.Count == 0)
-                {
-                    _logger.LogError(LogMessages.NOOBJECTCREATED, nameof(ProvinceData));
-                    _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
-                    continue;
-                }
-
-                _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
-                                       provinceDatas.Count,
-                                       nameof(ProvinceData));
-
-                IList<AverageProvinceData> averageProvinceDatas =
-                    new List<AverageProvinceData>();
-
-                _logger.LogInformation(LogMessages.CREATEOBJECT, 
-                                       nameof(AverageProvinceData));
-
-                foreach (ProvinceData provinceData in provinceDatas)
-                {
-                    var averageProvinceData = _averageProvinceDataFactory.CreateAverageProvinceData(provinceData);
-
-                    if (!averageProvinceData.Success)
-                    {
-                        _logger.LogError(LogMessages.ERRORSFOUND, errorGroupNumber);
-                        foreach (var errors in averageProvinceData.Errors)
-                        {
-                            _logger.LogError(errors);
-                            continue;
-                        }
-
-                        errorGroupNumber++;
-                    }
-
-                    else averageProvinceDatas.Add(averageProvinceData.Value);
-                }
-
-                if (averageProvinceDatas.Count == 0)
-                {
-                    _logger.LogError(LogMessages.NOOBJECTCREATED, nameof(AverageProvinceData));
-                    _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
-                    continue;
-                }
-
-                _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
-                                       nameof(AverageProvinceData),
-                                       averageProvinceDatas.Count);
-
-                _logger.LogInformation(LogMessages.EXPORTAVERAGEPROVINCEDATA);
-                using var fileStream = new FileStream(noErrorsFilePath,
-                                                        FileMode.Create,
-                                                        FileAccess.Write);
-
-                Log.CloseAndFlush();
-
-                _averageProvinceDataExportService.Export(averageProvinceDatas,
-                                                            fileStream);
+                else consistentReadings.Add(cr.Value);
             }
+
+            if (consistentReadings.Count == 0)
+            {
+                _logger.LogError(LogMessages.NOOBJECTCREATED, nameof(ConsistentReading));
+                _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
+                return (false, consistentReadings);
+            }
+
+            _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
+                                   consistentReadings.Count,
+                                   nameof(ConsistentReading));
+
+            return (true, consistentReadings);
+        }
+
+        public (bool Success, IList<ProvinceData> provinceDatas) CreateProvinceData(IList<ConsistentReading> consistentReadings)
+        {
+            _logger.LogInformation(LogMessages.CREATEOBJECT, nameof(ProvinceData));
+
+            IList<ProvinceData> provinceDatas =
+                _provinceDataListFactory.CreateProvinceDataList(consistentReadings);
+
+            if (provinceDatas.Count == 0)
+            {
+                _logger.LogError(LogMessages.NOOBJECTCREATED, nameof(ProvinceData));
+                _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
+                return (false, provinceDatas);
+            }
+
+            _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
+                                   provinceDatas.Count,
+                                   nameof(ProvinceData));
+
+            return (true, provinceDatas);
+        }
+
+        public (bool Success, IList<AverageProvinceData> averageProvinceDatas) CreateAverageProvinceData(IList<ProvinceData> provinceDatas)
+        {
+            IList<AverageProvinceData> averageProvinceDatas =
+                new List<AverageProvinceData>();
+
+            _logger.LogInformation(LogMessages.CREATEOBJECT,
+                                   nameof(AverageProvinceData));
+
+            foreach (ProvinceData provinceData in provinceDatas)
+            {
+                var averageProvinceData = _averageProvinceDataFactory.CreateAverageProvinceData(provinceData);
+
+                if (!averageProvinceData.Success)
+                {
+                    _logger.LogError(LogMessages.ERRORSFOUND, _errorGroupNumber);
+                    foreach (var errors in averageProvinceData.Errors)
+                    {
+                        _logger.LogError(errors);
+                        continue;
+                    }
+
+                    _errorGroupNumber++;
+                }
+
+                else averageProvinceDatas.Add(averageProvinceData.Value);
+            }
+
+            if (averageProvinceDatas.Count == 0)
+            {
+                _logger.LogError(LogMessages.NOOBJECTCREATED, nameof(AverageProvinceData));
+                _logger.LogInformation(LogMessages.CONTINUETONEXTFILE);
+                return (false, averageProvinceDatas);
+            }
+
+            _logger.LogInformation(LogMessages.OBJECTCREATIONSUCCESS,
+                                   nameof(AverageProvinceData),
+                                   averageProvinceDatas.Count);
+
+            return (true, averageProvinceDatas);
+        }
+
+        public void ExportAverageProvinceData(string noErrorsFilePath, IList<AverageProvinceData> averageProvinceDatas)
+        {
+            _logger.LogInformation(LogMessages.EXPORTAVERAGEPROVINCEDATA);
+            using var fileStream = new FileStream(noErrorsFilePath,
+                                                    FileMode.Create,
+                                                    FileAccess.Write);
+
+            Log.CloseAndFlush();
+
+            _averageProvinceDataExportService.Export(averageProvinceDatas,
+                                                        fileStream);
         }
 
         private string ExportDirectoryPathGeneratorAndLoggerConfiguration(string baseOutPath)
         {
-            string specificReadingsAndErrorsDirectoryName = 
+            string specificReadingsAndErrorsDirectoryName =
                 SystemTime.Now().ToString("yyyyMMdd_HHmmss");
 
             string fullFolderPath = Path.Combine(baseOutPath,
@@ -189,10 +229,10 @@ namespace SimulationExercise.Services
 
             Directory.CreateDirectory(fullFolderPath);
 
-            string noErrorsFilePath = Path.Combine(fullFolderPath, 
+            string noErrorsFilePath = Path.Combine(fullFolderPath,
                                                    "AverageProvinceData.csv");
 
-            string errorsFilePath = Path.Combine(fullFolderPath, 
+            string errorsFilePath = Path.Combine(fullFolderPath,
                                                  "Errors.log");
 
             LogPathHolder.ErrorLogPath = errorsFilePath;
